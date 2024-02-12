b0c490d344a7db420a4842c8c4f01282318ada2e
barkangel <xrebdognc@gmail.com>
2024-02-12 14:34:04 -0800

    fixed counter.py errors with flake8

--- src/counter.py
+++ src/counter.py
@@ -1,23 +1,17 @@
 from flask import Flask
+from src import status
 
 app = Flask(__name__)
 
-from src import status
-
-
 COUNTERS = {}
 
-# We will use the app decorator and create a route called slash counters.
-# specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
-# on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
@@ -40,4 +34,4 @@ def delete_counter(name):
     app.logger.info(f"Request to delete counter: {name}")
     if name in COUNTERS:
         COUNTERS.pop(name)
-        return {name:name},status.HTTP_204_NO_CONTENT
+        return {name: name}, status.HTTP_204_NO_CONTENT
