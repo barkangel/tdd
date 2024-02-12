bee82c311e1ba31ec1800299d44791b072b426db
barkangel <xrebdognc@gmail.com>
2024-02-12 14:14:32 -0800

    added tdd lab code and ci preliminary workflow code

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,58 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+from unittest import TestCase
+
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+class CounterTest(TestCase):
+    """Counter tests"""
+
+    def test_create_a_counter(self):
+     """It should create a counter"""
+     client = app.test_client()
+     result = client.post('/counters/foo')
+     self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        client = app.test_client()
+        result = self.client.post('/counters/counter1')
+        print("RESULT: ", result.data)
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED) #201 = Successful creation return code
+        self.assertEqual(b'{"counter1":0}\n', result.data)
+
+        updateResult = self.client.put('/counters/counter1')
+        self.assertEqual(updateResult.status_code, status.HTTP_200_OK) #200 = Ok
+        self.assertEqual(b'{"counter1":1}\n', updateResult.data)
+        self.assertNotEqual(updateResult.data, result.data)
+
+    def test_read_a_counter(self):
+        client = app.test_client()
+        result = self.client.post('/counters/counter_to_be_read')
+        getResult = self.client.get('/counters/counter_to_be_read')
+
+        self.assertEqual(getResult.status_code, status.HTTP_200_OK)
+        self.assertEqual(b'{"counter_to_be_read":0}\n', getResult.data)
+
+    def test_delete_a_counter(self):
+        """It should delete a counter"""
+        result = self.client.post('/counters/toBeDel')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        delResult = self.client.delete('/counters/toBeDel')
+        self.assertEqual(delResult.status_code, status.HTTP_204_NO_CONTENT)
+
+    def setUp(self):
+        self.client = app.test_client()
